70d67e23d2f3c0037cf7bed964dfb39afc1d01c1
Tyler D <tyler@tdaigle.com>
2022-10-30 06:46:22 -0400

    working exam endpoint

--- pages/api/exams.ts
+++ pages/api/exams.ts
@@ -1,11 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
 import { prisma } from "../../db_util/db";
+import { z } from "zod";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const exam = await prisma.exam.findFirst();
-  res.status(200).json(exam);
+  const examNameSchema = z.string();
+
+  try {
+    if ("exam" in req.query) {
+      const examName = req.query.exam?.toString();
+      examNameSchema.parse(examName);
+      const exam = await prisma.exam.findFirst({ where: { name: examName } });
+      if (exam) {
+        res.status(200).json(exam);
+        return;
+      }
+    }
+  } catch (e) {
+    res.status(404).json({});
+    return;
+  }
+  res.status(404).json({});
 }
