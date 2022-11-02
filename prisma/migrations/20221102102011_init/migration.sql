63e4466219e89a31005edd8ebdb3995f82eb7e5d
Tyler D <tyler@tdaigle.com>
2022-11-02 06:20:59 -0400

    fixing...

--- prisma/migrations/20221102102011_init/migration.sql
+++ prisma/migrations/20221102102011_init/migration.sql
@@ -1,7 +1,16 @@
 -- CreateTable
+CREATE TABLE "Exam" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "numberQuestions" INTEGER NOT NULL,
+    "requiredCorrect" INTEGER NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "Question" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "questionId" TEXT NOT NULL,
+    "questionText" TEXT NOT NULL,
     "answer" INTEGER NOT NULL,
     "subelement" TEXT NOT NULL,
     "group" TEXT NOT NULL,
@@ -10,6 +19,9 @@ CREATE TABLE "Question" (
     "choice2" TEXT NOT NULL,
     "choice3" TEXT NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Exam_name_key" ON "Exam"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Question_questionId_key" ON "Question"("questionId");
