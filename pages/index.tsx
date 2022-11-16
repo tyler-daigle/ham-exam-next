92827bb61d312b5d2f7efe1529f19ddc03d84fe2
Tyler D <tyler@tdaigle.com>
2022-11-16 03:24:07 -0500

    Updated ExamHeader style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,9 @@
-import Link from "next/link";
-import { prisma } from "../db_util/db";
+import ExamHeader from "@/components/ExamHeader";
 
-interface PageProps {
-  examIds: number[]
-}
-
-export default function Home({ examIds }: PageProps) {
-  console.log(examIds);
-
+export default function Index() {
   return (
-    <div>
-      <h1>Ham Exam</h1>
-      <Link href="/question/T1A01">Question</Link>
-      <Link href="/details">Details</Link>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, nobis!
-        Laboriosam eos necessitatibus laborum assumenda?
-      </p>
-      <h2>Technician Exams:</h2>
-      <ol>
-        {examIds.map(examId => <li key={examId}>
-          <Link href={`/exam/${examId}`}>Tech Exam {examId}</Link>
-        </li>)}
-      </ol>
-    </div>
-  );
-}
-
-export async function getStaticProps() {
-  const exams = await prisma.generatedExam.findMany({
-    where: {
-      examId: 0,
-    },
-    select: {
-      id: true
-    }
-  });
-
-  return {
-    props: {
-      examIds: exams.map(exam => exam.id)
-    }
-  }
+    <>
+      <ExamHeader>Amateur-Radio.School</ExamHeader>
+    </>
+  )
 }
