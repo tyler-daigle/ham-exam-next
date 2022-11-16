85cb80f0f8c7e31c289a3d60633097538058194c
Tyler D <tyler@tdaigle.com>
2022-11-16 04:44:45 -0500

    ExamSelector working

--- components/ExamSelector.tsx
+++ components/ExamSelector.tsx
@@ -1,26 +1,41 @@
+import { useState } from "react";
+import Link from "next/link";
 import styles from "@/styles/ExamSelector.module.css";
 
 import Button from "./UI/Button";
 
 export default function ExamSelector() {
+  const [techVisible, setTechVisible] = useState(false);
+  const [generalVisible, setGeneralVisible] = useState(false);
+  const [extraVisible, setExtraVisible] = useState(false);
+
   return (
     <ul className={styles.selectorContainer}>
       <li className={styles.examItem}>
-        <Button>Technician</Button>
+        <Button onClick={() => setTechVisible(!techVisible)}>Technician</Button>
         <p className={styles.examDetails}>
           This is the first level of amateur license.
         </p>
+
+        <div className={techVisible ? `${styles.examDirections} ${styles.examDirectionsToggle}` : styles.examDirections}>
+          <p>
+            There are <strong>35 questions</strong> on the Technician Exam. You must get at least <strong>26 correct</strong> in order
+            to pass.
+          </p>
+          <Link className={styles.examLink} href="/exam">Click here to begin the exam.</Link>
+        </div>
+
       </li>
 
       <li className={styles.examItem}>
-        <Button>General</Button>
+        <Button onClick={() => setGeneralVisible(!generalVisible)} disabled>General</Button>
         <p className={styles.examDetails}>
           <strong>General</strong> is your next step after <strong>Technician</strong>.
         </p>
       </li>
 
       <li className={styles.examItem}>
-        <Button>Extra</Button>
+        <Button onClick={() => setExtraVisible(!extraVisible)} disabled>Extra</Button>
         <p className={styles.examDetails}>
           <strong>Extra</strong> is obtainable after <strong>General</strong>.
         </p>
--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,9 +1,14 @@
 import styles from "@/styles/Button.module.css";
 
 export interface Props {
-  children: React.ReactNode
+  children: React.ReactNode;
+  onClick: React.MouseEventHandler;
+  disabled?: boolean;
 }
 
-export default function Button({ children }: Props) {
-  return <button className={styles.primaryButton}>{children}</button>;
+export default function Button({ children, onClick, disabled }: Props) {
+  if (disabled) {
+    return <button onClick={(e) => e.preventDefault()} className={`${styles.button} ${styles.disabledButton}`}>{children}</button>;
+  }
+  return <button onClick={onClick} className={`${styles.button} ${styles.primaryButton}`}>{children}</button>;
 }
